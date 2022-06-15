23a3245477def2d87986af13bda5b03394f5c2e2
ArS3Ny <93928890+ArS3Ny@users.noreply.github.com>
2022-06-16 01:13:08 +0300

    э

--- images_convert.py
+++ images_convert.py
@@ -1,23 +1,17 @@
 import numpy as np
 from PIL import Image
 
-count = 0
-name = input('введите конечное имя файлов ')
-for count in range(50):
+for count in range(1, 59):
     try:
         with Image.open(f'images/theory_{count}.png') as im:
             im = im.convert('RGBA')
             data = np.array(im)
-            rgb = data[:, :, :3]
-            color = [255, 255, 255]
-            black = [0, 0, 0, 255]
-            white = [255, 255, 255, 255]
-            mask = np.all(rgb == color, axis=-1)
-            data[mask] = [0, 0, 0, 0]
-            data[np.logical_not(mask)] = black
+            rgba = data[:, :, :4]
+            mask = np.all(rgba == [0, 0, 0, 0], axis=-1)  # c какого цвета
+            data[mask] = [0, 0, 0, 0]  # на какой цвет
+            data[np.logical_not(mask)] = [255, 255, 255, 150]
             new_im = Image.fromarray(data)
-            new_im.save(f'new_images/{name}_{count}.png')
-
+            new_im.save(f'new_images/theory_{count}.png')  # вместо theory название конечных файлов
     except:
         pass
 

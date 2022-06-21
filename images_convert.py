0d1ea59545e1fccd3bef59f2bc6512df9d276d05
ArS3Ny <93928890+ArS3Ny@users.noreply.github.com>
2022-06-21 21:34:30 +0300

    удаление плоскостей ораньжевых

--- images_convert.py
+++ images_convert.py
@@ -1,18 +1,20 @@
 import numpy as np
 from PIL import Image
+from PyQt5.QtWidgets import QLabel, QCheckBox
 
 for count in range(1, 59):
     try:
-        with Image.open(f'images/theory_{count}.png') as im:
+        with Image.open(f'images/img_{count}.png') as im:
             im = im.convert('RGBA')
             data = np.array(im)
             rgba = data[:, :, :4]
-            mask = np.all(rgba == [0, 0, 0, 0], axis=-1)  # c какого цвета
+            rgb = data[:, :, :3]
+            mask = np.all(rgba == [255, 255, 255, 255], axis=-1)  # c какого цвета
+            mask2 = np.all(rgb == [255, 196, 128], axis=-1)
             data[mask] = [0, 0, 0, 0]  # на какой цвет
-            data[np.logical_not(mask)] = [255, 255, 255, 150]
+            data[mask2] = [0, 0, 0, 0]
+            data[np.logical_not(np.logical_or(mask, mask2))] = [255, 255, 255, 150]
             new_im = Image.fromarray(data)
-            new_im.save(f'new_images/theory_{count}.png')  # вместо theory название конечных файлов
+            new_im.save(f'new_images/exer_{count}.png')  # вместо theory название конечных файлов
     except:
         pass
-
-
